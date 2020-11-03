803a917610aeba5aff9b97f76547ab0b68f4bc4c
Ismail Al Ghani <ismailalghani@gmail.com>
2020-11-03 14:33:33 +0700

    remove google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { MenuController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router } from '@angular/router';
-import { Environment } from '@ionic-native/google-maps';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -68,10 +67,6 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      Environment.setEnv({
-        'API_KEY_FOR_BROWSER_RELEASE': environment.googleMapsAPIKey,
-        'API_KEY_FOR_BROWSER_DEBUG': environment.googleMapsAPIKey  // optional
-      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -7,7 +7,6 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
-import { GoogleMapsComponent } from 'src/app/components/google-maps/google-maps.component';
 import { CameraComponent } from 'src/app/components/camera/camera.component';
 import { CapGeolocComponent } from 'src/app/components/cap-geoloc/cap-geoloc.component';
 import { AuthFingerFaceComponent } from 'src/app/components/auth-finger-face/auth-finger-face.component';
@@ -19,6 +18,6 @@ import { AuthFingerFaceComponent } from 'src/app/components/auth-finger-face/aut
     IonicModule,
     ProfilePageRoutingModule
   ],
-  declarations: [ProfilePage, GoogleMapsComponent, CameraComponent, CapGeolocComponent, AuthFingerFaceComponent]
+  declarations: [ProfilePage, CameraComponent, CapGeolocComponent, AuthFingerFaceComponent]
 })
 export class ProfilePageModule {}
